29adfe18d0c79d76195d52d4dff386a01e8e4e90
Joseph Ibochi <josephibochi@gmail.com>
2025-07-29 23:04:24 +0100

    update cache

--- src/context/axios-context.tsx
+++ src/context/axios-context.tsx
@@ -4,6 +4,8 @@ export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL ?? "",
     headers: {
         "Content-Type": "application/json",
-        Accept: 'application/json'
+        Accept: 'application/json',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
     },
 })
